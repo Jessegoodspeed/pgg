9bb7bae98c828398d9c0588b71d8af5e97e2f2fc
Jesse <Jessegoodspeed@gmail.com>
2020-05-29 03:26:03 -0400

    Finished preliminary draft of 'classes' file

--- classes.py
+++ classes.py
@@ -1,7 +1,6 @@
-class Player:
+import pandas as pd
 
-    # Class Attribute
-    # prev_avg_conts = list()
+class Player:
 
     # Initializer / Instance Attributes
     def __init__(self, beta1, beta2, discount, initialCont=5):
@@ -14,8 +13,9 @@ class Player:
         return f'Player({self.b1!r}, {self.b2!r}, {self.disc!r}, {self.ic!r},\
                         {self.cont_hx!r})'
     # Contribute (and add value to contribution hx) --> int
-    def contribute(self, numOfRounds):
+    def contribute(self, avg_prev_contributions=1, numOfRounds=10):
         # arguments: round t, prev contribution, average of previous round's contributions
+        # set default value for avg_prev_contributions = 1 to avoid runtime error for initial contributions
         # return value cannot be less than 0 nor greater than 10
         if len(self.cont_hx) is 0:
             self.cont_hx.append(self.ic)
@@ -30,14 +30,60 @@ class Player:
     def get_hx(self):
         return self.cont_hx
 
-class Round:
-    # each player contributes
-    # compute average of avg of contributions
-    pass
+class Roster:
+    def __init__(self):
+        self._roster = []
+
+    def add_player(self, beta1, beta2, discount, initialOffering=5):
+        self._roster.append(Player(beta1, beta2, discount, initialOffering))
 
 class PGG_Instance:
     # Number of rounds (default = 10)
-    # Number of players (default = 5)
-    # List of average contributions for each round?
+    def __init__(self, roster_of_players, numOfRounds=10):
+        self._roster_list = roster_of_players
+        self.totalRounds = numOfRounds
+        self.currentRound = 0
+        self.avg_contributions = []
+        self.active_status = True
+
+    def __repr__(self):
+        return f'PGG_Instance({len(self._roster_list)!r} players, rounds: \
+                {self.totalRounds!r}, current round: {self.currentRound!r}, \
+                avg_contribs: {self.avg_contributions!r}, active: \
+                {self.active_status!r})'
+
+    def initialization(self):
+        self.currentRound = 1
+        contribs = []
+        for i in self._roster_list:
+            contribs.append(i.contribute())
+        avg_contrib = sum(contribs) / len(self._roster_list)
+        self.avg_contributions.append(avg_contrib)
+
+    def next_round(self):
+        if self.currentRound < self.totalRounds:
+            self.currentRound += 1
+            contribs = []
+            for i in self._roster_list:
+                contribs.append(i.contribute(self.avg_contributions[-1], \
+                                            self.totalRounds))
+            avg_contrib = sum(contribs) / len(self._roster_list)
+            self.avg_contributions.append(avg_contrib)
+        else:
+            self.active_status = False
+
     # Snapshot: append data to csv file that maintains data generated for this run
-    pass
+    def create_instance_df(self):
+        params={}
+        contributions={}
+        for i, player in enumerate(self._roster_list,1):
+            params[f'player {i} beta1'] = [player.b1 for i in self.totalRounds]
+            params[f'player {i} beta2'] = [player.b2 for i in self.totalRounds]
+            params[f'player {i} discount'] = [player.disc for i in \
+                                        self.totalRounds]
+            contributions[f'player {i} contributions'] = player.get_hx()
+        dfp = pd.DataFrame(params, index=list(range(1,11)))
+        dfc = pd.DataFrame(contributions, index=list(range(1,11)))
+        df = pd.concat([dfp,dfc], axis=1)
+        df['Round #'] = list(range(1,11))
+        return df
