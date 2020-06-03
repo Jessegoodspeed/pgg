31ccd52ed715603d7b93e2f6800f139d1418a99e
Jesse <Jessegoodspeed@gmail.com>
2020-06-03 14:09:28 -0400

    Added logging statement and cleaned up some comments.

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 '''
-Possible start:
 
-    implement a simple model from Watts paper
-    stochastic 2-step model with discount factor for impulsivity - 3 parameters per player
-    variables: n, beta 1, beta 2, discount factor, and initial contribution
-
-How should we handle initial contribution? How do other social experiment papers handle initial contribution?
-Model output - determine what the variance of output is?
-Which output format would be the best for our purposes? -- we will try CSV to use for statistical analysis via R
-
-    21 columns: 15 for parameter input, 5 for each player contribution, and 1 column to denote round
-    10 rows, where each row is a single round
-
-Let's define an instance as 5 agents playing 10 rounds of PGG
-Let's run each instance k times, and set k = 1000 - so that we have sufficient data to generate some decent statistical analysis
-20 random instances: 10 completely random instances, 10 instances that are less random and more deliberate to explore parameter space somehow
-
-    example to explore discount factor - fix beta 1 and beta 2 and randomize the discount factor
 '''
 
 import pandas as pd
@@ -27,27 +10,26 @@ import random
 Roster = classes.Roster
 PGG_Instance = classes.PGG_Instance
 
-k = 10
+k = 1000
 numOfRounds = 10
-
 k_instances = {}
 k_dfs = {}
 
-#beta1: (0,1.25)
-#beta2: (0,1.25)
-#discount: (0,1)
-
-player_types = ['TF', 'DTF']
+player_types = ['TF', 'DTF'] # List of player models
 for i in range(k):
     test = Roster()
+
+    # Parameters are randomly chosen
     beta1_dist = np.random.default_rng().uniform(0, 1.25, 5)
     beta2_dist = np.random.default_rng().uniform(0, 1.25, 5)
     disc_dist = np.random.default_rng().uniform(0, 1, 5)
     type_choice = player_types[random.randint(0,1)]
 
+    # Game players are initialized
     for (b1,b2,disc) in zip(beta1_dist,beta2_dist,disc_dist):
         test.add_player((b1,b2,disc), type=type_choice)
-    print(test._roster)
+
+    # Instance is initialized
     inst = PGG_Instance(test, numOfRounds)
     inst.initialization()
     while(inst.active_status):
@@ -59,5 +41,8 @@ for i in range(k):
         continue
     else:
         k_dfs[f'df_0'] = pd.concat([k_dfs[f'df_0'],k_dfs[f'df_{i}']])
+
+    if i%100 is 0:
+        print(f'{i} instances completed!')
 df = k_dfs[f'df_0'].reset_index(drop=True)
-df.to_csv('test_csv.csv')
+df.to_csv('k_1000.csv')
